a9352fd9f1ecca1db805346c3c9cb1051f6f63ea
HimeshRoy <himeshroy.cse@gmail.com>
2026-07-30 22:39:37 +0530

    bugs fixed

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -21,4 +21,22 @@ api.interceptors.request.use(
   (error) => Promise.reject(error),
 );
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      const store = useAuthStore.getState() as any;
+      
+      if (store.logout) {
+        store.logout();
+      } else {
+        useAuthStore.setState({ token: null, user: null });
+      }
+      window.location.href = "/login";
+    }
+    
+    return Promise.reject(error);
+  },
+);
+
 export default api;
